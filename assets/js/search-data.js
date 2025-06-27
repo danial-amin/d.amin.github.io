b2b6a3ee95e03c18d43b37f2210eee9d76b1e95a
danial-amin <writetodanialamin@gmail.com>
2025-06-27 20:17:34 +0000

    Deploying to gh-pages from @ danial-amin/danial-amin.github.io@e6487ad508df558ddd46ef0388cfacbe40fdd60e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -512,14 +512,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%72%69%74%65%74%6F%64%61%6E%69%61%6C%61%6D%69%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/danial-amin", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/danialamin", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0000-7597-2267", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,14 +547,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=anC0OlUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
