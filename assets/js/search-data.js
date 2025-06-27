ec3f9a684f016cb8cfee7e95650aeabb33bae00c
danial-amin <writetodanialamin@gmail.com>
2025-06-27 19:54:27 +0000

    Deploying to gh-pages from @ danial-amin/danial-amin.github.io@dce977fba623689b9a85f37c879a15aa8ea879f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-ai-ethics",
+        
+          title: "Ai Ethics",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/AI-Ethics/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
